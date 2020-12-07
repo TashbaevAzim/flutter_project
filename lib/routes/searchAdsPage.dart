25b538835cc827c224406e48b40976ced9bbc90a
Tashbaev Azim <tashbaev_az@auca.kg>
2020-12-08 02:29:33 +0600

    created car cards in Объявления

--- lib/routes/searchAdsPage.dart
+++ lib/routes/searchAdsPage.dart
@@ -1,4 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:senior_project/carInfo.dart';
+
+import '../style.dart';
+import 'car_details_page.dart';
 
 class SearchAdsPage extends StatefulWidget {
   @override
@@ -6,6 +12,8 @@ class SearchAdsPage extends StatefulWidget {
 }
 
 class _SearchAdsPageState extends State<SearchAdsPage> {
+  List<CarInfo> carAdsList = CarInfo.list;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,15 +30,94 @@ class _SearchAdsPageState extends State<SearchAdsPage> {
       //   ],
       // ),
 
-      body: Container(
-        alignment: Alignment.center,
-        child: Text(
-          "На этой странице будут объявления.",
-          style: TextStyle(
-            fontSize: 17,
-          ),
-        ),
-      ) //
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 10,
+                      top: 10,
+                    ),
+                    itemCount: carAdsList.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.all(15),
+                        margin: EdgeInsets.only(
+                          bottom: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          color: kTextLightColor,
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context){return CarDetailsPage(carAdsList[index]);
+                            }),
+                            );
+                            },
+                            //                            onTap: () {Navigator.of(context).push(MaterialPageRoute (builder: (_) => CarDetailsPage(carList[index]),
+                            child: Row(
+                              children: <Widget>[
+                                Image(
+                                  width: 65,
+                                  image: AssetImage("assets/images/carIconCard.png"),
+                                ),
+                                SizedBox(width: 15),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "Ищу владельца авто",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      "${carAdsList[index].govNumber}",
+                                      style: TextStyle(
+                                          fontSize: 18
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(width: 25),
+                                Icon(
+                                    Icons.keyboard_arrow_right,
+                                    size: 35
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              )
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue[800],
+      ),
     );
   }
 }
--- lib/routes/search_auto_result.dart
+++ lib/routes/search_auto_result.dart
@@ -37,67 +37,7 @@ class SearchAutoResult extends StatelessWidget {
                     ),
                     itemCount: carList.length,
                     itemBuilder: (context, index) {
-                    return Container(
-                        padding: EdgeInsets.all(15),
-                        margin: EdgeInsets.only(
-                          bottom: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          color: kTextLightColor,
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 3,
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context){return CarDetailsPage(carList[index]);
-                            }),
-                            );
-                            },
-                            //                            onTap: () {Navigator.of(context).push(MaterialPageRoute (builder: (_) => CarDetailsPage(carList[index]),
-                            child: Row(
-                              children: <Widget>[
-                                Image(
-                                  width: 65,
-                                  image: AssetImage("assets/images/carIconCard.png"),
-                                ),
-                                SizedBox(width: 15),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      "Информация об авто",
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    SizedBox(height: 5),
-                                    Text(
-                                      "${carList[index].govNumber}",
-                                      style: TextStyle(
-                                        fontSize: 18
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(width: 25),
-                                Icon(
-                                  Icons.keyboard_arrow_right,
-                                  size: 35
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                    );
+                    return CarContainer(context, index);
                   },),
                 ],
               )
@@ -107,5 +47,69 @@ class SearchAutoResult extends StatelessWidget {
 
     );
 
+  }
+
+  Container CarContainer(BuildContext context, int index) {
+    return Container(
+                      padding: EdgeInsets.all(15),
+                      margin: EdgeInsets.only(
+                        bottom: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: kTextLightColor,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context){return CarDetailsPage(carList[index]);
+                          }),
+                          );
+                          },
+                          //                            onTap: () {Navigator.of(context).push(MaterialPageRoute (builder: (_) => CarDetailsPage(carList[index]),
+                          child: Row(
+                            children: <Widget>[
+                              Image(
+                                width: 65,
+                                image: AssetImage("assets/images/carIconCard.png"),
+                              ),
+                              SizedBox(width: 15),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Информация об авто",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "${carList[index].govNumber}",
+                                    style: TextStyle(
+                                      fontSize: 18
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 25),
+                              Icon(
+                                Icons.keyboard_arrow_right,
+                                size: 35
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                  );
   }
 }
